b60486ea2e05f593d7e4542cdf345cd0a59b2cb3
Sumitgupta22 <sumitgupta969139@gmail.com>
2024-03-28 04:23:14 +0000

    add input and output file

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,41 @@
 // import static org.junit.jupiter.api.Assertions.assertEquals;
 
 // import org.junit.jupiter.api.Test;
-
+import java.io.*;
+class f_System{
+  File infile;
+  File outfile;
+  FileReader in;
+  FileWriter out;
+void Prossecefile (String infilename,String outfilename)throws IOException{
+   infile = new File(infilename);
+   outfile = new File(outfilename);
+   in = new FileReader(infile);
+   out = new FileWriter(outfile);
+   int c;
+   while((c=in.read())!=-1){
+     out.write(c);
+   }
+ }
+}
 public class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    System.out.println("welcome to File System!");
+    f_System fs = new f_System();
+    try{
+      fs.Prossecefile("in.txt","out.txt");
+    }catch(IOException e){
+      System.out.println("Error"+ e);
+    }
+    finally {
+      try{
+        fs.in.close();
+        fs.out.close();
+      }catch(IOException e){
+        System.out.println("Error"+ e);
+      }
+      
+    }
   }
 
   // @Test
